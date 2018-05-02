3e3b94af1d0d788574e0bd2790dc168bc58a6541
Omar Hesham <omar_abdellatif97@hotmail.com>
2018-05-02 11:09:36 +0200

    remove node_modules

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -3,6 +3,10 @@ var img;
 var tank={}, tank2;
 var tanks = [];
 var bullets = [];
+var dead = false;
+var fire = false;
+var canFire = true;
+var isOverCircle = false;
 
 // var vertices = [[65, 40], [48, 40], [48, 41], [47, 41], [47, 42], [46, 42], [46, 43], [45, 43], [45, 44], [44, 44], [44, 45], [43, 45], [43, 46], [42, 46], [42, 47], [41, 47], [41, 48], [39, 48], [39, 49], [37, 49], [37, 50], [34, 50], [34, 51], [29, 51], [29, 52], [24, 52], [24, 51], [19, 51], [19, 50], [16, 50], [16, 49], [14, 49], [14, 48], [12, 48], [12, 47], [11, 47], [11, 46], [10, 46], [10, 45], [9, 45], [9, 44], [8, 44], [8, 43], [7, 43], [7, 42], [6, 42], [6, 41], [5, 41], [5, 39], [4, 39], [4, 38], [3, 38], [3, 35], [2, 35], [2, 32], [1, 32], [1, 20], [2, 20], [2, 17], [3, 17], [3, 15], [4, 15], [4, 13], [5, 13], [5, 11], [6, 11], [6, 10], [7, 10], [7, 9], [8, 9], [8, 8], [9, 8], [9, 7], [10, 7], [10, 6], [11, 6], [11, 5], [13, 5], [13, 4], [14, 4], [14, 3], [16, 3], [16, 2], [19, 2], [19, 1], [34, 1], [34, 2], [36, 2], [36, 3], [39, 3], [39, 4], [40, 4], [40, 5], [42, 5], [42, 6], [43, 6], [43, 7], [44, 7], [44, 8], [45, 8], [45, 9], [46, 9], [46, 10], [47, 10], [47, 11], [48, 11], [48, 12], [65, 12]];
 // var vertices = [[-50,-50], [50,-50], [50,50], [-50,50]];
@@ -15,6 +19,13 @@ var bullets = [];
 // vertex(100,50);
 // vertex(0,50);
 
+
+// setInterval(function(){
+//     // if(tanks.has(socket.id))
+//     canFire=true;
+// },200);
+
+
 function findTank(element) {
     return element.id == socket.id;
 }
@@ -25,15 +36,27 @@ function preload()
 function setup() {
     createCanvas(windowWidth, windowHeight);
     socket.on('tanks', function (data) {
-        tanks = data.tanks;
+        tanks = new Map(data.tanks);
         bullets = data.bullets;
-        var index = tanks.findIndex(findTank);
-        tank.x = tanks[index].x;
-        tank.y = tanks[index].y;
-		tanks.splice(index,1);
+        // var index = tanks.findIndex(findTank);
+        if(tanks.has(socket.id))
+        {
+            tank.score = tanks.get(socket.id).score;
+            tank.x = tanks.get(socket.id).x;
+            tank.y = tanks.get(socket.id).y;
+            tank.health = tanks.get(socket.id).health;
+            tanks.delete(socket.id);
+            dead = false;
+        }
+        else
+        {
+            dead = true;
+        }
+        
     });
     imageMode(CENTER);
     rectMode(CENTER);
+    ellipseMode(RADIUS);
     frameRate(60);
 }
 
@@ -43,20 +66,26 @@ function fire() {
 }
 function myshow(t) {
     push();
+    // console.log(t.score);
     translate(t.x, t.y);
-
-    var drawWidth = (t.health / 100) * 60;
+    // console.log(t.health);
+    var drawWidth = (t.health / 100) * 50;
     fill(0,0,0);
-    rect(0,-40,50,10);
+    rect(0,-40,drawWidth,10);
+    // stroke(255,0,0);
+    // noFill();
+    // rect(0,-40,50,10);
+    // ellipseMode(RADIUS)
     rotate((t.angle));
     image(img, 0, 0);
+    // ellipse(0,0,20,20);
     pop();
 }
 function myshow2(c) {
     push();
     fill(255,0,0);
     translate(c.x, c.y);
-    ellipse(0,0,20,20);
+    ellipse(0,0,10,10);
     pop();
 }
 function update() {
@@ -69,28 +98,91 @@ function update() {
     if (keyIsDown(68)) a++;
     socket.emit('move',{x:a,y:b,angle:angle});
 	tank.angle = angle;
-    if (keyIsDown(75)) {
+    if ((keyIsDown(75)||fire===true) && canFire === true) {
         console.log('fire');
         socket.emit('fire',{});
+        canFire = false;
+        setTimeout(function(){
+                canFire = true;
+            }, 120);
     }
+}
+function died(){
+
+  // background(backgroundColor);
+ 
+  // get distance between mouse and circle
+  var distance = dist(mouseX - (width / 2), mouseY - ( height / 2), 0, 0); 
+  
+  // if the distance is less than the circle's radius
+  if(distance < 100 * (width/1366))
+  {
+    isOverCircle = true;
+  } else {
+    isOverCircle = false;
+  }
+  
+  // draw a circle
+  // ellipseMode(CENTER);
+  stroke(0);
+  strokeWeight(5);
+  if(isOverCircle == true)
+  {
+    fill(100);
+    cursor(HAND);
+  } else {
+    fill(200); 
+    cursor(ARROW); 
+  }
+  ellipse(0, 0, 100, 100);
+  
+}
+ 
+function mousePressed()
+{
+  if(isOverCircle == true && dead == true)
+  {
+    fill(200); 
+    cursor(ARROW);
+    socket.emit('continue_playing',{});
+
+    // backgroundColor = color(random(255), random(255), random(255));
+  }
 }
 function draw(){
     // if(tank!={})
-    if(Object.keys(tank).length)
+    background(222, 201, 255);
+    translate(width / 2, height / 2);
+    scale(width/1366);
+    if(Object.keys(tank).length&&!dead)
     {
-
-        background(255);
-        translate(width / 2, height / 2);
-        scale(width/1366);
         translate(-tank.x, -tank.y);
         update();
         myshow(tank);
         for(var i=0;i<tanks.length;i++)
             myshow(tanks[i]);
+
+        for(let [pid, t] of tanks){
+                myshow(t);
+                // ctx.fillStyle = "purple";
+                // circle(player.x,player.y,10);
+            }
+
         for(var i=0;i<bullets.length;i++)
             myshow2(bullets[i]);
+    }
+    if(dead)
+    {
+
+        background(255);
+        died();
     }
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+function keyPressed() {
+  if (keyCode === 84) {
+    fire = !fire;
+  }
 }
--- index.js
+++ index.js
@@ -2,9 +2,19 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var tanks = [];
 var bullets = [];
+var tanks = new Map();
+var killedTanks = new Map();
+var qt = require('./quadtree');
 
+
+var width = 2000,height = 2000;
+
+
+
+
+
+console.log(typeof qt.QuadTree);
 
 function Tank(x, y, angle, id, health) {
     this.x = x;
@@ -12,6 +22,8 @@ function Tank(x, y, angle, id, health) {
     this.angle = angle;
     this.id = id;
     this.health = health;
+    this.canFire = true;
+    this.score = 0;
 }
 
 function Bullet(x, y, angle, id) {
@@ -21,41 +33,163 @@ function Bullet(x, y, angle, id) {
     this.id = id;
 }
 
+function dist(t,b)
+{
+    var x1,x2,y1,y2;
+    x1 = t.x; x2 = b.x;
+    y1 = t.y; y2 = b.y;
+    var a = x1 - x2;
+    var b = y1 - y2;
+
+    var c = Math.sqrt( a*a + b*b );
+    return c;
+    // return Math.sqrt(Math.pow(2,(y2-y1)))
+}
+
+
+
+function findBullet(element) {
+        return element.id == this;
+    }
+
+
+
 setInterval(function one() {
-    io.emit('tanks', {tanks:tanks,bullets:bullets});
+    let boundary = new qt.Rectangle(0, 0, width / 2, height / 2);
+    let qtree = new qt.QuadTree(boundary, 4);
+    io.emit('tanks', {tanks:Array.from(tanks),bullets:bullets});
 	for(var i = bullets.length - 1;i>=0;i--)
     {
         bullets[i].x+=10*Math.cos(bullets[i].angle);
         bullets[i].y+=10*Math.sin(bullets[i].angle);
         if(Math.abs(bullets[i].x)>=2000||Math.abs(bullets[i].y)>=2000)
             bullets.splice(i,1);
+        else
+        {
+            let point = new qt.Point(bullets[i].x, bullets[i].y,  bullets[i] , i);
+            qtree.insert(point); 
+        }
     }
-}, 40);
+    for(let [pid, t] of tanks){
+        // console.log(t.id);
+        // console.log(t.health);
+        // console.log("\n");
+
+        let range = new qt.Circle(t.x, t.y, 500);
+
+        let points = qtree.query(range);
+
+        // console.log(points);
+        for (let point of points) {
+
+            let other = point.userData;
+
+            if (t.id != other.id) {
+                if(dist(t,other)<=20+10)
+                {
+                    t.health-=10;
+                    // let index = bullets.findIndex(findBullet);
+                    // for(var i = bullets.length - 1;i>=0;i--)
+                    // {
+                    //     if(bullets[i].id==other.id)
+                    //     {
+                    //         bullets.splice(i,1);
+                    //     }
+                    // }
+                    // console.log(dist(t,other));
+                    // console.log(point.index);
+                  	bullets.splice(point.index,1);
+                    
+
+                    // var index = bullets.findIndex(findBullet,other.id);
+                    // console.log(index);
+                    // bullets.splice(index,1);
+                    // let index2 = points.findIndex(x => x.id==other.id)
+                    // console.log("omar \n")
+                    // console.log(other.id);
+                    // let 
+                    // let index2 = points.findIndex(x => x.userData.id==other.id);
+                    // console.log(index);
+                    // console.log(index2);
+                    // points.splice(index2,1);
+                    if(t.health<=0)
+                    {
+                    	if(tanks.has(other.id))
+				        {
+				            tanks.get(other.id).score = tanks.get(other.id).score + 1;
+				        }
+				        killedTanks.set(pid,tanks.get(pid));
+                        tanks.delete(pid);
+                        break;
+                    }
+                }
+            }
+        }
+        /*for(var i = bullets.length - 1;i>=0;i--)
+        {
+            if(dist(t,bullets[i])<=40&&(bullets[i].id!=t.id))
+            {
+                t.health-=10;
+                bullets.splice(i,1);
+                if(t.health<=0)
+                {
+                    tanks.delete(pid);
+                }
+            }
+        }  */    
+    }
+}, 1000/60);
 
 
 io.on('connection', function (socket) {
-    function findTank(element) {
-        return element.id == socket.id;
-    }
-
+    // setInterval(function(){
+    //     tanks.get(socket.id).canFire=true;
+    // },400);
     console.log('A user connected');
     console.log(socket.id);
-    tanks.push(new Tank(0, 0, 0, socket.id,100));
+    tanks.set(socket.id,new Tank(0, 0, 0, socket.id,100));
+
     socket.on('disconnect', function () {
-        var index = tanks.findIndex(findTank);
-        tanks.splice(index,1);
+        tanks.delete(socket.id);
         console.log('A user disconnected');
     });
     socket.on('move', function (data) {
-        var index = tanks.findIndex(findTank);
-        tanks[index].x += data.x;
-        tanks[index].y += data.y;
-        tanks[index].angle = data.angle;
+        if(tanks.has(socket.id))
+        {
+            tanks.get(socket.id).x += data.x*4;
+            tanks.get(socket.id).y += data.y*4;
+            tanks.get(socket.id).angle = data.angle;
+        }
     });
+
+
+socket.on('continue_playing', function (data) {
+        if(killedTanks.has(socket.id))
+        {
+        	var mytank = killedTanks.get(socket.id);
+        	mytank.health = 100;
+            tanks.set(socket.id,mytank);
+            killedTanks.delete(socket.id);
+        }
+    });
+
+
     socket.on('fire', function (data) {
-        var index = tanks.findIndex(findTank);
-        var bullet = new Bullet(tanks[index].x,tanks[index].y,tanks[index].angle,socket.id);
-        bullets.push(bullet);
+        if(tanks.has(socket.id)&&tanks.get(socket.id).canFire)
+        {	
+
+
+        	// 10*Math.cos(bullets[i].angle);
+			// 10*Math.sin(bullets[i].angle);
+            var bullet = new Bullet(tanks.get(socket.id).x + 50*Math.cos(tanks.get(socket.id).angle),tanks.get(socket.id).y + 50*Math.sin(tanks.get(socket.id).angle),tanks.get(socket.id).angle,socket.id);
+            bullets.push(bullet);
+            tanks.get(socket.id).canFire = false;
+            // tanks
+            setTimeout(function(){
+                if(tanks.has(socket.id))
+                    tanks.get(socket.id).canFire=true;
+            },200);
+        }
     });
 });
 
@@ -66,6 +200,6 @@ app.get('/', function (req, res) {
     res.sendfile('./client/index.html');
 });
 
-http.listen(process.env.PORT || 2000, function () {
+http.listen(3000, function () {
     // console.log('listening on localhost:2000');
 });
